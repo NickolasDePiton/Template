ebfc8d6c4686d9f9cc0a584aab943b63ae2918b6
NickolasDePiton <crysis4997@ya.ru>
2016-04-06 13:44:18 +0400

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -1,34 +1,33 @@
-#ifndef Matrix_h
-#define Matrix_h
+#pragma once
+#ifndef matrix_h
+#define matrix_h
 
 #include <iostream>
-#include <stdlib.h>
 #include <fstream>
+#include <string>
 using namespace std;
-template < typename SomeValueType> class Matrix  
+
+template < typename SomeValueType> class Matrix 
 {
-private:
-	int lines;
-	int columns;
-	SomeValueType **matrix;
 public:
-	int cout_columns() const;
-	int cout_lines() const;
-	Matrix();
-	Matrix(const Matrix &a);
+	Matrix() :n(0), m(0), matrix(nullptr) {}
+	Matrix(int lines, int columns);
+	Matrix(const Matrix& copy);
 	~Matrix();
-	Matrix(int _lines, int _columns);
-	void read_matrix(std::string s);
-	void print_matrix() const;
-	Matrix operator + (const Matrix &array);
-	Matrix operator * (const Matrix &array);
-	SomeValueType* operator [](int i) const;
-	Matrix &operator = (Matrix &a);
-	void reset();
-	bool operator ==(const Matrix &array) const;
+	Matrix operator + (const Matrix&);
+	Matrix operator - (const Matrix&);
+	Matrix operator * (const Matrix&);
+	Matrix &operator = (const Matrix&);
+	bool operator == (const Matrix&);
+	SomeValueType* operator [] (int);
+	int Lines() const;
+	int Columns() const;
 	friend ostream &operator << (ostream &cout, const Matrix &temp);
 	friend istream &operator >> (istream &input, Matrix &matr);
-
+private:
+	SomeValueType **matrix;
+	int n;
+	int m;	
 };
 
 #endif
