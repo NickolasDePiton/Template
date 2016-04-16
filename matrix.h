1c4a9a6d698c2ce70a2e62ab5bc9267758dc5716
NickolasDePiton <crysis4997@ya.ru>
2016-04-16 18:39:22 +0300

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -1,33 +1,51 @@
 #pragma once
-#ifndef matrix_h
-#define matrix_h
-
+#include "stdafx.h"
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <string.h>
+
 using namespace std;
 
-template < typename SomeValueType> class Matrix 
+
+template <typename T>
+class matrix;
+
+template <class T>
+std::ostream & operator<<(std::ostream & output, const matrix<T> &);
+
+template <class T>
+std::istream & operator>>(std::istream & input, matrix<T> &);
+template <typename T>
+class matrix
 {
 public:
-	Matrix() :n(0), m(0), matrix(nullptr) {}
-	Matrix(int lines, int columns);
-	Matrix(const Matrix& copy);
-	~Matrix();
-	Matrix operator + (const Matrix&);
-	Matrix operator - (const Matrix&);
-	Matrix operator * (const Matrix&);
-	Matrix &operator = (const Matrix&);
-	bool operator == (const Matrix&);
-	SomeValueType* operator [] (int);
-	int Lines() const;
-	int Columns() const;
-	friend ostream &operator << (ostream &cout, const Matrix &temp);
-	friend istream &operator >> (istream &input, Matrix &matr);
+        matrix<T>();
+matrix(int _collumns, int _lines);
+	matrix(const matrix &Matrix);
+	matrix(T **matr, unsigned int _n, unsigned int _m);
+	matrix(T **matr, unsigned int _n, unsigned int _m, int time);
+	~matrix<T>();
+	void get_line(int k);
+	void get_from_file(string _name);
+	void print_matrix() const;
+	void copy_matrix(const matrix &Matrix);
+	matrix &operator=(const matrix &Matrix);
+	friend matrix operator+(const matrix &matrix_1, const matrix &matrix_2);
+	friend matrix operator*(const matrix &matrix_1, const matrix &matrix_2);
+	T *operator[] (int i) const;
+	void get_sum_r(const matrix &matrix_1, const matrix &matrix_2, int s);
+	void get_multi_r(const matrix &matrix_1, const matrix &matrix_2, int m);
+	void create_memory();
+	int get_num_rows() const;
+	int get_num_cols() const;
+	int get(int rows, int columns) const;
+	void set(int row, int columns, int set);
+	friend std::ostream & operator<< (std::ostream & output, const matrix<T> & x);
+	friend std::istream & operator>> (std::istream & input, matrix<T> & _matrix);
 private:
-	SomeValueType **matrix;
-	int n;
-	int m;	
+	int rows;
+	int columns;
+	T **_matrix;	
 };
 
 #endif
